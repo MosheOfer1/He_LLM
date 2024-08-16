a363e5a5f3d8c5e00778307371c0f4ba6523fbdb
Moshe Ofer <mosheofer1@gmail.com>
2024-08-16 14:25:41 +0300

    translator works

--- llm/llm_integration.py
+++ llm/llm_integration.py
@@ -1,53 +1,40 @@
 from transformers import AutoTokenizer, OPTForCausalLM
 import torch
-import torch.nn as nn
+from utilty.injectable import CustomLayerWrapper, Injectable
 
 
-class CustomLayerWrapper(nn.Module):
-    def __init__(self, layer, hidden_states):
-        super().__init__()
-        self.layer = layer
-        self.hs = hidden_states  # The injected hidden state layer
-
-    def forward(self, hidden_states, attention_mask=None, layer_head_mask=None,
-                past_key_value=None, output_attentions=None, use_cache=None):
-        # Apply modifications to hidden_states here
-
-        # Pass modified_hidden_states to the original layer
-        return self.layer(self.hs, attention_mask, layer_head_mask,
-                          past_key_value, output_attentions, use_cache)
-
-
-class LLMIntegration:
+class LLMIntegration(Injectable):
     def __init__(self, model_name):
         """
         Initialize the LLMIntegration with a specific OPT model.0
         """
+        self.outputs = None
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = OPTForCausalLM.from_pretrained(model_name)
         self.model_name = model_name
 
-        # Let the LLM be Injectable by replacing the first layer of the LLM
-        original_layer = self.model.base_model.decoder.layers[0]
+        # Let the LLM be Injectable by replacing the first block of the LLM
+        self.injected_layer_num = 0
+        original_layer = self.model.base_model.decoder.layers[self.injected_layer_num]
         wrapped_layer = CustomLayerWrapper(original_layer, None)
-        self.model.base_model.decoder.layers[0] = wrapped_layer
+        self.model.base_model.decoder.layers[self.injected_layer_num] = wrapped_layer
 
-    def inject_hs(self, layer_num, llm_first_hs: torch.Tensor):
+    def inject_hidden_states(self, injected_hidden_state: torch.Tensor):
         """
-        Inject hidden states into the LLM by using a custom layer that wrappers the origin first layer of the LLM: CustomLayerWrapper.
+        Inject hidden states into the LLM by using a custom layer that wrappers the origin first layer
+        of the LLM: CustomLayerWrapper.
 
-        :param inputs_embeds: The input embeddings to be injected into the LLM.
-        :return: The logits layer from the LLM.
+        :param injected_hidden_state: The injected hidden states
         """
-        self.model.base_model.decoder.layers[layer_num].hs = llm_first_hs
+        self.model.base_model.decoder.layers[self.injected_layer_num].injected_hidden_state = injected_hidden_state
 
-    def get_output(self, token_num=5):
+    def get_output_by_using_dummy(self, token_num):
         # Generate a dummy input for letting the model output the desired result of the injected layer
         inputs = self.tokenizer(" " * (token_num - 1), return_tensors="pt")
 
-        outputs = self.model(**inputs, output_hidden_states=True)
+        self.outputs = self.model(**inputs, output_hidden_states=True)
 
-        return self.decode_logits(outputs.logits)
+        return self.outputs
 
     def process_text_input_to_logits(self, text: str) -> torch.Tensor:
         """
@@ -77,12 +64,12 @@ class LLMIntegration:
         return generated_text
 
     @staticmethod
-    def text_to_first_hs(text, model_name):
+    def text_to_hidden_states(text, layer_num, model_name):
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         model = OPTForCausalLM.from_pretrained(model_name)
 
         inputs = tokenizer(text, return_tensors="pt")
         outputs = model(**inputs, output_hidden_states=True)
 
-        return outputs.hidden_states[0]
+        return outputs.hidden_states[layer_num]
 
