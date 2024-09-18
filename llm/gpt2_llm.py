4e433ea995634ff535525e454427715ef24096f1
Moshe Ofer <mosheofer1@gmail.com>
2024-09-18 23:57:07 +0300

    env vars

--- llm/gpt2_llm.py
+++ llm/gpt2_llm.py
@@ -7,7 +7,7 @@ from llm.llm_wrapper import LLMWrapper
 
 
 class GPT2LLM(LLMWrapper):
-    def __init__(self, model_name: str, device: str = 'cpu'):
+    def __init__(self, model_name: str, device='cpu'):
         # Initialize tokenizer and model
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         llm_model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
